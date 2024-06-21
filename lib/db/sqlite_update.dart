518d14a8ca057ba09b53935092e4f6498c34a1af
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-06-21 20:01:18 -0300

    Alteracao deletar banco e atualizar

--- lib/db/sqlite_update.dart
+++ lib/db/sqlite_update.dart
@@ -9,46 +9,52 @@ class DatabaseUpdater {
   static Future<void> syncData(Database db) async {
 
     final String? token = await getToken();
-    if(token ==null){
+    if(token == null) {
       throw Exception('Login inválido');
     }
     final headers = {
       'Authorization': 'Bearer $token',
     };
 
-    //verificado id
-    var lastEmployeeId = Sqflite.firstIntValue(await db.rawQuery('SELECT MAX(id) FROM Funcionario')) ?? 0;
-    var lastTreinamentoId = Sqflite.firstIntValue(await db.rawQuery('SELECT MAX(id) FROM Treinamento')) ?? 0;
+    // Verificar IDs
+    var lastEmployeeId = Sqflite.firstIntValue(await db.rawQuery('SELECT MAX(id) FROM funcionario')) ?? 0;
+    var lastTreinamentoId = Sqflite.firstIntValue(await db.rawQuery('SELECT MAX(id) FROM treinamento')) ?? 0;
 
-    var responseFuncionarios = await http.get(Uri.parse("http://localhost:8080/funcionario?idGreaterThan=$lastEmployeeId"),headers: headers);
+    var responseFuncionarios = await http.get(Uri.parse("http://localhost:8080/funcionario?idGreaterThan=$lastEmployeeId"), headers: headers);
     var dataFuncionarios = json.decode(responseFuncionarios.body);
 
-    var responseTreinamentos = await http.get(Uri.parse("http://localhost:8080/treinamento?idGreaterThan=$lastTreinamentoId"),headers: headers);
+    var responseTreinamentos = await http.get(Uri.parse("http://localhost:8080/treinamento?idGreaterThan=$lastTreinamentoId"), headers: headers);
     var dataTreinamentos = json.decode(responseTreinamentos.body);
 
-    var responseFuncionarioTreinamento = await http.get(Uri.parse("http://localhost:8080/funcionariotreinamento"),headers: headers);
+    var responseFuncionarioTreinamento = await http.get(Uri.parse("http://localhost:8080/funcionariotreinamento"), headers: headers);
     var dataFuncionarioTreinamento = json.decode(responseFuncionarioTreinamento.body);
 
-    //sqlite conexão
     await db.transaction((txn) async {
-      dataFuncionarios.forEach((funcionario) async {
-        await txn.rawInsert(
-            'INSERT INTO Funcionario(id, nome, setor, cargo, inscricao) VALUES(?, ?, ?, ?, ?)',
-            [funcionario['id'], funcionario['nome'], funcionario['setor'], funcionario['cargo'],funcionario['inscricao']]);
-      });
 
-      dataTreinamentos.forEach((treinamento) async {
-        await txn.rawInsert(
-            'INSERT INTO Treinamento(id, nome, classificacao, inicio, fim,  descricao) VALUES(?, ?, ?, ?, ?, ?)',
-            [treinamento['id'], treinamento['nome'], treinamento['descricao'], treinamento['inicio'], treinamento['fim'], treinamento['classificacao']]);
-      });
+      await txn.execute('DELETE FROM funcionario');
+      await txn.execute('DELETE FROM treinamento');
+      await txn.execute('DELETE FROM funcionario_treinamento');
 
-      dataFuncionarioTreinamento.forEach((funcionarioTreinamento) async {
+      for (var funcionario in dataFuncionarios) {
+        await txn.rawInsert(
+            'INSERT INTO funcionario(id, nome, setor, cargo, inscricao) VALUES(?, ?, ?, ?, ?)',
+            [funcionario['id'], funcionario['nome'], funcionario['setor'], funcionario['cargo'], funcionario['inscricao']]
+        );
+      }
+
+      for (var treinamento in dataTreinamentos) {
+        await txn.rawInsert(
+            'INSERT INTO treinamento(id, nome, classificacao, inicio, fim, descricao) VALUES(?, ?, ?, ?, ?, ?)',
+            [treinamento['id'], treinamento['nome'], treinamento['classificacao'], treinamento['inicio'], treinamento['fim'], treinamento['descricao']]
+        );
+      }
+
+      for (var funcionarioTreinamento in dataFuncionarioTreinamento) {
         await txn.rawInsert(
             'INSERT INTO funcionario_treinamento(funcionario_id, treinamento_id) VALUES(?, ?)',
             [funcionarioTreinamento['funcionarioDTO']['id'], funcionarioTreinamento['treinamentoDTO']['id']]
         );
-      });
+      }
     });
   }
 }
