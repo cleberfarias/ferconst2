33e22c5a303d70d678e9df176ce02e2d871fea74
cleberfarias <cleber_afd@hotmail.com>
2024-06-24 21:34:39 -0300

    layouts

--- lib/src/cadastro/cadastro_page.dart
+++ lib/src/cadastro/cadastro_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:intl/intl.dart';
 
-
 import '../../model/data/employeeModel.dart';
 import '../../model/repositories/errors/api_exception.dart';
 import '../../model/repositories/implementations/dio_api_repository.dart';
@@ -98,25 +97,64 @@ class _CadastroPageState extends State<CadastroPage> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Data da inscrição:',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 8.0),
-                        TextFormField(
-                          readOnly: true,
-                          controller: inscricaoController,
-                          decoration: InputDecoration(
-                            hintText: 'Escolha uma data',
-                            labelText: 'Data da inscrição',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Data da inscrição:',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  TextFormField(
+                                    readOnly: true,
+                                    controller: inscricaoController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Escolha uma data',
+                                      labelText: 'Data da inscrição',
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      suffixIcon: IconButton(
+                                        onPressed: _showDatePicker,
+                                        icon: Icon(Icons.search),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                            suffixIcon: IconButton(
-                              onPressed: _showDatePicker,
-                              icon: Icon(Icons.search),
+                            SizedBox(width: 20.0),
+                            Expanded(
+                              flex: 9,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Cargo:',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  TextField(
+                                    controller: cargoController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Digite o cargo',
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                         SizedBox(height: 20.0),
                         Text(
@@ -134,21 +172,6 @@ class _CadastroPageState extends State<CadastroPage> {
                           ),
                         ),
                         SizedBox(height: 20.0),
-                        Text(
-                          'Cargo:',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 8.0),
-                        TextField(
-                          controller: cargoController,
-                          decoration: InputDecoration(
-                            hintText: 'Digite o cargo',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 20.0),
                         Row(
                           children: [
                             Expanded(
@@ -157,13 +180,15 @@ class _CadastroPageState extends State<CadastroPage> {
                                 children: [
                                   Text(
                                     'Setor:',
-                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
                                   ),
                                   SizedBox(height: 8.0),
                                   DropdownButtonFormField<String>(
                                     decoration: InputDecoration(
                                       border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10.0),
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
                                       ),
                                     ),
                                     items: [
@@ -242,20 +267,24 @@ class _CadastroPageState extends State<CadastroPage> {
 
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     SnackBar(
-                                      content: Text('Cadastro realizado com sucesso!'),
+                                      content: Text(
+                                          'Cadastro realizado com sucesso!'),
                                     ),
                                   );
                                 } on ApiException catch (e) {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     SnackBar(
-                                      content: Text('Cadastro realizado - Erro interno: ${e.menssagem}'),
+                                      content: Text(
+                                          'Cadastro realizado - Erro interno: ${e.menssagem}'),
                                     ),
                                   );
-                                  print("Erro ao enviar o cadastro: ${e.menssagem}");
+                                  print(
+                                      "Erro ao enviar o cadastro: ${e.menssagem}");
                                 } catch (e) {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     SnackBar(
-                                      content: Text('Erro ao enviar o cadastro: $e'),
+                                      content:
+                                          Text('Erro ao enviar o cadastro: $e'),
                                     ),
                                   );
                                   print("Erro ao enviar o cadastro: $e");
